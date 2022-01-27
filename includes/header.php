8b7dd6c591ca533aa67b76ebf19b63d8607aca1b
Kimberleeeey <kimdelgado215@gmail.com>
2022-01-27 17:11:56 +0800

    Update UI

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   	<!-- Font Awesome -->
   	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/v4-shims.css">
   	<!-- Theme style -->
   	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   	<!-- AdminLTE Skins -->
@@ -36,66 +37,68 @@
     <!-- Google Recaptcha -->
     <script src='https://www.google.com/recaptcha/api.js'></script>
 
-  	<!-- Custom CSS -->
+    <!-- Custom CSS -->
     <style type="text/css">
-    
-    /* Small devices (tablets, 768px and up) */
-    @media (min-width: 768px){ 
-      #navbar-search-input{ 
-        width: 60px; 
+      
+      /* Small devices (tablets, 768px and up) */
+      @media (min-width: 768px){ 
+        #navbar-search-input{ 
+          width: 60px; 
+        }
+        #navbar-search-input:focus{ 
+          width: 100px; 
+        }
       }
-      #navbar-search-input:focus{ 
-        width: 100px; 
+
+      /* Medium devices (desktops, 992px and up) */
+      @media (min-width: 992px){ 
+        #navbar-search-input{ 
+          width: 150px; 
+        }
+        #navbar-search-input:focus{ 
+          width: 250px; 
+        } 
       }
-    }
 
-    /* Medium devices (desktops, 992px and up) */
-    @media (min-width: 992px){ 
-      #navbar-search-input{ 
-        width: 150px; 
+      *{
+        font-family: 'Poppins', sans-serif;
       }
-      #navbar-search-input:focus{ 
-        width: 250px; 
-      } 
-    }
 
-    *{
-      font-family: 'Poppins', sans-serif;
-    }
-    .word-wrap{
-      overflow-wrap: break-word;
-    }
-    .prod-body{
-      height:300px;
-    }
+      .word-wrap{
+        overflow-wrap: break-word;
+      }
+      .prod-body{
+        height:300px;
+      }
 
-    .box:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    }
-    .register-box{
-      margin-top:20px;
-    }
+      .box:hover{
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+      }
+      .register-box{
+        margin-top:20px;
+      }
 
-    #trending{
-      list-style: none;
-      padding:10px 5px 10px 15px;
-    }
-    #trending li {
-      padding-left: 1.3em;
-    }
-    #trending li:before {
-      content: "\f046";
-      font-family: FontAwesome;
-      display: inline-block;
-      margin-left: -1.3em; 
-      width: 1.3em;
-    }
+      #trending{
+        list-style: none;
+        padding:10px 5px 10px 15px;
+      }
+      #trending li{
+        padding-left: 1.3em;
+        padding-bottom: 1em;
+      }
+      #trending li:before{
+        content: "\f14a";
+        font-family: "Font Awesome 5 Free";
+        display: inline-block;
+        margin-left: -1.3em; 
+        width: 1.3em;
+      }
 
-    /*Magnify*/
-    .magnify > .magnify-lens {
-      width: 100px;
-      height: 100px;
-    }
+      /*Magnify*/
+      .magnify > .magnify-lens{
+        width: 100px;
+        height: 100px;
+      }
 
     </style>
 
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="box box-solid">
-	  	<div class="box-header with-border">
-	    	<h3 class="box-title"><b>Most Viewed Today</b></h3>
+	  	<div class="box-header">
+	    	<h3 class="box-title">Most Viewed Today</h3>
 	  	</div>
 	  	<div class="box-body">
 	  		<ul id="trending">
@@ -22,36 +22,16 @@
 	</div>
 </div>
 
-<div class="row">
-	<div class="box box-solid">
-	  	<div class="box-header with-border">
-	    	<h3 class="box-title"><b>Become a Subscriber</b></h3>
-	  	</div>
-	  	<div class="box-body">
-	    	<p>Get free updates on the latest products and discounts, straight to your inbox.</p>
-	    	<form method="POST" action="">
-		    	<div class="input-group">
-	                <input type="text" class="form-control">
-	                <span class="input-group-btn">
-	                    <button type="button" class="btn btn-info btn-flat"><i class="fa fa-envelope"></i> </button>
-	                </span>
-	            </div>
-		    </form>
-	  	</div>
-	</div>
-</div>
-
-<div class="row">
-	<div class='box box-solid'>
-	  	<div class='box-header with-border'>
-	    	<h3 class='box-title'><b>Follow us on Social Media</b></h3>
-	  	</div>
-	  	<div class='box-body'>
-	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
-	    	<a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
-	    	<a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
-	    	<a class="btn btn-social-icon btn-google"><i class="fa fa-google-plus"></i></a>
-	    	<a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
-	  	</div>
-	</div>
-</div>
+<style>
+	.box{
+		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+	}
+	.box-header{
+		background-color: gray;
+		color: white;
+		text-align: center;
+	}
+	.box-header h3{
+		font-family: 'Poppins', sans-serif;
+	}
+</style>
